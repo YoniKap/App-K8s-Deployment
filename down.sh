5fe963005cd4d183cf292e73bb4eb97e43bb7dde
Yonatan Kaplonov <yonikas123@gmail.com>
2023-07-28 15:30:29 +0300

    added secrets

--- down.sh
+++ down.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+kubectl delete -f  appsecrets.yml
 kubectl delete -f application.yml 
 kubectl delete -f  authdb.yml
 kubectl delete -f guitardb.yml
--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+kubectl apply -f  appsecrets.yml 
 kubectl apply -f  application.yml 
 kubectl apply -f authdb.yml
 kubectl apply -f guitardb.yml
